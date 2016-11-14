b8c1d73355424bc159342dfe39250ea91068bb17
mitsuan <sumit.cooldude00@gmail.com>
2016-11-14 09:34:08 +0530

    Update message2short_msg.py

--- dataProcessing/message2short_msg.py
+++ dataProcessing/message2short_msg.py
@@ -1,3 +1,5 @@
+#Coverts a normal text to short message form eliminating some vowels
+#E.g Python---> Pythn, Sunset--->Snst, Colorful---> Clrfl, What are you doing?--->Wht are you dng?
 str=input('enter ur msg: ')
 l=str.split(' ')
 vowels=['a','e','i','o','u']
@@ -5,7 +7,7 @@ msg=[]
 #print(l)
 for i in l:
    # print(len(i))
-    if(len(i)>3):
+    if(len(i)>3): #Conversion is done only if the length of the word is > 3. 
         #print(len(i))
         for j in range(0,len(i)):
             if(j==0 or j==(len(i)-1)or i[j] not in vowels or (j!=(len(i)-1) and i[j+1]==',')):
